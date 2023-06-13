44b7d2b7643dc6b13298188206f0a011d185a434
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-13 15:56:36 +0700

    🎨 style(layout.tsx): wrap body tag in Head component This commit wraps the body tag in the Head component, which is a Next.js component that allows for the manipulation of the head of the document. This improves the structure of the code and makes it easier to read.

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,20 +34,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-          </ThemeProvider>
-        </body>
+        <Head>
+          <body
+            className={cn(
+              "min-h-screen bg-background font-sans antialiased",
+              fontSans.variable
+            )}
+          >
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <div className="relative flex min-h-screen flex-col">
+                <SiteHeader />
+                <div className="flex-1">{children}</div>
+              </div>
+            </ThemeProvider>
+          </body>
+        </Head>
       </html>
     </>
   )
