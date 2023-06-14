92bef712a766066f02687e545df79202a62f8fca
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-14 12:46:05 +0700

    🚀 chore(settings.json): update typescript.tsdk path 🆕 feat(dashboard): add knowledge page The knowledge page is added to the dashboard. It is a simple page that displays a heading. The page is located at app/(dashboard)/dashboard/knowledge/page.tsx.
    🆕 feat(header.tsx): add DashboardHeader component
    The DashboardHeader component is added to the project. It is a reusable component that displays a heading and an optional description. It is located at components/header.tsx.
    
    🆕 feat(shell.tsx): add DashboardShell component
    The DashboardShell component is added to the project. It is a reusable component that provides a grid layout for the dashboard. It is located at components/shell.tsx.
    
    🆕 feat(ui/card.tsx): add Card, CardHeader, CardFooter, CardTitle, CardDescription, and CardContent components
    The Card, CardHeader, CardFooter, CardTitle, Card

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,16 @@
+import { Card } from "@/components/ui/card"
+import { DashboardHeader } from "@/components/header"
+import { DashboardShell } from "@/components/shell"
+
 export default function DashboardPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Knowledge
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          This is the knowledge page.
-        </p>
-      </div>
-    </section>
+    <DashboardShell>
+      <DashboardHeader
+        heading="Welcome back, Jane"
+        description="Here’s what’s happening with your projects today"
+      />
+
+      <Card className="flex-1">Hello</Card>
+    </DashboardShell>
   )
 }
