61576c47cc628392e493bad389a6b984784155fa
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-16 11:09:29 +0700

    🎨 style(columns.tsx): add dropdown menu to the actions column to improve user experience 🎨 style(data-table.tsx): add pagination buttons to improve user experience 🎨 style(data-table.tsx): add sorting and column visibility state to improve user experience 🎨 style(data-table.tsx): add no results message to improve user experience 🎨 style(data-table.tsx): add arrowUpDown icon to improve user experience 🎨 style(icons.tsx): add arrowUpDown icon to improve user experience

--- app/(dashboard)/dashboard/category/columns.tsx
+++ app/(dashboard)/dashboard/category/columns.tsx
@@ -3,6 +3,16 @@
 import { ColumnDef } from "@tanstack/react-table"
 
 import { DataCategory } from "@/types/category-res"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Icons } from "@/components/icons"
 
 /**
  * Definition of columns for the DataCategory table.
@@ -14,6 +24,45 @@ export const columns: ColumnDef<DataCategory>[] = [
   },
   {
     accessorKey: "category_name",
-    header: "Nama Kategori",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Nama Kategori
+          <Icons.arrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    header: "Aksi",
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <Icons.moreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+            // onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
