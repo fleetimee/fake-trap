ad8f040885b81977d4a1565b35efeffab509cc31
Novian Andika <hello@fleetime.my.id>
2023-06-18 14:48:59 +0700

    🎨 style(page.tsx): refactor DetailKnowledge component to use DetailContent and DetailSidebarKnowledge components The DetailKnowledge component has been refactored to use the DetailContent and DetailSidebarKnowledge components. This improves the readability and maintainability of the code by separating the concerns of the component into smaller, more focused components.

--- app/(dashboard)/dashboard/knowledge/[detail]/page.tsx
+++ app/(dashboard)/dashboard/knowledge/[detail]/page.tsx
@@ -1,25 +1,5 @@
-import Image from "next/image"
-
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Icons } from "@/components/icons"
+import { DetailContent } from "@/components/detail-content"
+import { DetailSidebarKnowledge } from "@/components/detail-sidebar-knowledge"
 import { DashboardShell } from "@/components/shell"
 
 export default function DetailKnowledge({
@@ -29,120 +9,9 @@ export default function DetailKnowledge({
 }) {
   return (
     <DashboardShell>
-      <div className="flex h-auto flex-col gap-4 px-2 md:flex-row">
-        <Card className="flex basis-3/4 items-start justify-normal">
-          <div className="flex flex-col gap-6 p-4">
-            <div className="flex flex-row items-center justify-between">
-              <p className="grow break-all font-heading text-3xl">
-                {params.detail}
-              </p>
-              <Icons.bookmark className="h-14 w-14 flex-none  pl-5" />
-            </div>
-            <Image
-              src="https://pbs.twimg.com/media/FyzcwvqaYAEqDjq?format=jpg&name=small"
-              alt="nigger"
-              className="aspect-video rounded-lg object-cover sepia hover:sepia-0"
-              width={1280}
-              height={720}
-            />
-            <Tabs defaultValue="description" className="hidden md:block">
-              <TabsList className="grid w-1/2 grid-cols-2">
-                <TabsTrigger value="description">Deskripsi</TabsTrigger>
-                <TabsTrigger value="users">Users</TabsTrigger>
-              </TabsList>
-              <TabsContent value="description">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Deskripsi</CardTitle>
-                    <CardDescription>Lorem ipsum</CardDescription>
-                  </CardHeader>
-                  <CardContent className="space-y-2">
-                    <ScrollArea className="h-[300px] w-full">
-                      <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Fermentum posuere urna nec tincidunt
-                        praesent semper. Eu volutpat odio facilisis mauris sit.
-                        Eget lorem dolor sed viverra. Diam maecenas ultricies mi
-                        eget mauris pharetra et. Fermentum iaculis eu non diam.
-                        Nibh sed pulvinar proin gravida hendrerit lectus. Vitae
-                        congue eu consequat ac felis donec et odio pellentesque.
-                        Nisl vel pretium lectus quam id leo in vitae. Faucibus
-                        interdum posuere lorem ipsum dolor sit. Quis imperdiet
-                        massa tincidunt nunc pulvinar sapien et ligula
-                        ullamcorper. Eget Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit, sed do eiusmod tempor
-                        incididunt ut labore et dolore magna aliqua. Fermentum
-                        posuere urna nec tincidunt praesent semper. Eu volutpat
-                        odio facilisis mauris sit. Eget lorem dolor sed viverra.
-                        Diam maecenas ultricies mi eget mauris pharetra et.
-                        Fermentum iaculis eu non diam. Nibh sed pulvinar proin
-                        gravida hendrerit lectus. Vitae congue eu consequat ac
-                        felis donec et odio pellentesque. Nisl vel pretium
-                        lectus quam id leo in vitae. Faucibus interdum posuere
-                        lorem ipsum dolor sit. Quis imperdiet massa tincidunt
-                        nunc pulvinar sapien et ligula ullamcorper. Eget
-                      </p>
-                    </ScrollArea>
-                  </CardContent>
-                </Card>
-              </TabsContent>
-              <TabsContent value="users">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Password</CardTitle>
-                    <CardDescription>
-                      Change your password here. After saving, youll be logged
-                      out.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                      <Label htmlFor="current">Current password</Label>
-                      <Input id="current" type="password" />
-                    </div>
-                    <div className="space-y-1">
-                      <Label htmlFor="new">New password</Label>
-                      <Input id="new" type="password" />
-                    </div>
-                  </CardContent>
-                  <CardFooter>
-                    <Button>Save password</Button>
-                  </CardFooter>
-                </Card>
-              </TabsContent>
-            </Tabs>
-          </div>
-        </Card>
-        <Card className="sticky flex h-[750px] basis-1/4 flex-col items-center justify-start">
-          <Tabs defaultValue="account" className="w-full">
-            <TabsList className="flex w-full">
-              <TabsTrigger value="account" className="w-full font-semibold">
-                📑 Pengetahuan
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">
-              <ScrollArea className="h-[650px]  w-full">
-                <Accordion type="single" collapsible className="px-4">
-                  {Array.from({ length: 20 }, (_, index) => (
-                    <AccordionItem
-                      key={index}
-                      value={`item-${index}`}
-                      className="text-sm"
-                    >
-                      <AccordionTrigger className="font-heading text-base font-bold">
-                        Pengetahuan {index + 1}
-                      </AccordionTrigger>
-                      <AccordionContent className="py-4">
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                      </AccordionContent>
-                    </AccordionItem>
-                  ))}
-                </Accordion>
-              </ScrollArea>
-            </TabsContent>
-          </Tabs>
-        </Card>
+      <div className="flex h-auto flex-col gap-4 px-2 lg:flex-row">
+        <DetailContent title={params.detail} />
+        <DetailSidebarKnowledge />
       </div>
     </DashboardShell>
   )
