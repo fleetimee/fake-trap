f19da79216b0a9104668eba8b43522e0fef463a2
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-19 16:42:33 +0700

    ✨ feat(create-section-button.tsx): add CreateSectionButton component to allow users to create a new section for a knowledge article. The component includes a Sheet modal with a form to input the section name and username.

---
+++ components/create-section-button.tsx
@@ -0,0 +1,55 @@
+import React from "react"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { CreateButton } from "@/components/knowledge-create-button"
+
+export function CreateSectionButton() {
+  return (
+    <div className="flex w-full justify-end py-4 pr-4">
+      <Sheet>
+        <SheetTrigger asChild>
+          <CreateButton name="Section" />
+        </SheetTrigger>
+        <SheetContent position="right" size="sm">
+          <SheetHeader>
+            <SheetTitle>Tambah Section</SheetTitle>
+            <SheetDescription>
+              Tambah section baru untuk pengetahuan ini.
+            </SheetDescription>
+          </SheetHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input id="username" value="@peduarte" className="col-span-3" />
+            </div>
+          </div>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Save changes</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  )
+}
