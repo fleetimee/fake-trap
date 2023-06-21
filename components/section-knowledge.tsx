a390b0193ca5897a61818af66befb7639ad74e5b
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-21 11:18:05 +0700

    ✨ feat(create-section-content-sheet.tsx): create a new component to add knowledge content to a section 🔨 refactor(section-knowledge.tsx): import CreateKnowledgeContentSheet from the new file name ✨ feat(ui/textarea.tsx): create a new component to render a textarea input field

--- components/section-knowledge.tsx
+++ components/section-knowledge.tsx
@@ -20,7 +20,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
-import { CreateKnowledgeContentSheet } from "@/components/create-knowledge-content-sheet"
+import { CreateKnowledgeContentSheet } from "@/components/create-section-content-sheet"
 
 enum ContentType {
   Video = 1,
