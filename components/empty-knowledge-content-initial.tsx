df2ce9cd4f9a70312f4a70669af2e10e1a87555e
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-21 11:17:48 +0700

    🔥 chore(create-knowledge-content-sheet.tsx): remove unused imports and variables 🚚 chore(empty-knowledge-content-initial.tsx): rename create-knowledge-content-sheet to create-section-content-sheet to match file name and component name

--- components/empty-knowledge-content-initial.tsx
+++ components/empty-knowledge-content-initial.tsx
@@ -11,7 +11,7 @@ import {
   ContextMenuTrigger,
 } from "@/components/ui/context-menu"
 import { Sheet, SheetTrigger } from "@/components/ui/sheet"
-import { CreateKnowledgeContentSheet } from "@/components/create-knowledge-content-sheet"
+import { CreateKnowledgeContentSheet } from "@/components/create-section-content-sheet"
 import { EmptyContent } from "@/components/detail-sidebar-empty-content"
 
 export function EmptyContentInitial({ id_section }: { id_section: number }) {
