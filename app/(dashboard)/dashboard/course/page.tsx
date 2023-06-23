6e697f65b1bc5f80fb916e4081fc791d5307ad2b
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-23 16:17:06 +0700

    🎨 style(course): refactor course page to use CourseGrid component and remove unused imports 🆕 feat(course-grid): add CourseGrid component to display course data in a grid format 🆕 feat(course-hover-content): add CourseHoverContent component to display course details on hover using HoverCard component from Radix UI 📦 chore(package.json): add @radix-ui/react-hover-card dependency to use HoverCard component in CourseHoverContent component

--- app/(dashboard)/dashboard/course/page.tsx
+++ app/(dashboard)/dashboard/course/page.tsx
@@ -1,19 +1,6 @@
-import { get } from "http"
-import Image from "next/image"
-
-import { Knowledge } from "@/types/knowledge-res"
 import { getCourse } from "@/lib/fetcher/course/course-fetcher"
 import { getKnowledge } from "@/lib/fetcher/knowledge/knowledge-fetcher"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { CreateButton } from "@/components/create-button"
+import { CourseGrid } from "@/components/course-grid"
 import { CreateCourseButton } from "@/components/create-course-sheet"
 import { DashboardHeader } from "@/components/header"
 import { DashboardShell } from "@/components/shell"
@@ -42,25 +29,7 @@ export default async function CoursePage() {
       </DashboardHeader>
       <div className="grid grid-cols-1 grid-rows-3 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {courseResp.data.map((item) => (
-          <Card
-            className="flex flex-col items-start justify-center hover:bg-accent hover:text-accent-foreground"
-            key={item.id_course}
-          >
-            <CardHeader className="h-22 w-full flex-none">
-              <p className="line-clamp-2 font-heading text-lg lg:text-xl">
-                {item.course_name}
-              </p>
-            </CardHeader>
-            <CardContent className="flex-none">
-              <Image
-                src={item.image}
-                alt="Picture of the author"
-                width={1200}
-                height={1200}
-                className="aspect-video flex-none rounded-lg object-cover"
-              />
-            </CardContent>
-          </Card>
+          <CourseGrid data={item} dataKnowledge={knowledgeResp} />
         ))}
       </div>
     </DashboardShell>
---
+++ components/course-hover-content.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { CourseData } from "@/types/course-res"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { HoverCardContent } from "@/components/ui/hover-card"
+import { Icons } from "@/components/icons"
+
+export function CourseHoverContent(props: { data: CourseData }) {
+  function convertDatetoString(date: string): string {
+    const dateObj = new Date(date)
+    return dateObj.toLocaleDateString("id-ID", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })
+  }
+
+  return (
+    <HoverCardContent className="w-80">
+      <div className="flex justify-between space-x-4">
+        <Avatar className="w-1/6">
+          <AvatarImage src={props.data.image} />
+          <AvatarFallback>VC</AvatarFallback>
+        </Avatar>
+        <div className="w-5/6 space-y-1">
+          <h4 className="text-sm font-semibold">Detail Kursus</h4>
+          <p className="text-sm">{props.data.course_desc}</p>
+          <div className="flex items-center pt-2">
+            <Icons.calendar className="mr-2 h-4 w-4 opacity-70" />{" "}
+            <span className="text-xs text-muted-foreground">
+              Mulai {convertDatetoString(props.data.date_start.toString())}
+            </span>
+          </div>
+          <div className="flex items-center pt-2">
+            <Icons.bookmark className="mr-2 h-4 w-4 opacity-70" />{" "}
+            <span className="text-xs text-muted-foreground">
+              Selesai {convertDatetoString(props.data.date_end.toString())}
+            </span>
+          </div>
+        </div>
+      </div>
+    </HoverCardContent>
+  )
+}
