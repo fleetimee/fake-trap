00ff9cc8a7de890717c7121a582852a11b322768
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-23 07:59:10 +0700

    🎨 style(globals.css): update color variables to improve readability and accessibility 🔧 chore(components.json): update baseColor to match new color scheme 🔧 chore(package.json): add @radix-ui/react-icons dependency 🔧 chore(tailwind.config.js): add src folder to content array to include styles from there 🔧 chore(tailwind.config.js): update borderRadius values to use string instead of var() function 🔧 chore(utils.ts): change order of import statements to match alphabetical order

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,11 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
-/**
- * Merges class names using Tailwind's twMerge and clsx utilities.
- * @param inputs - An array of class names to be merged.
- * @returns A string of merged class names.
- */
+ 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
