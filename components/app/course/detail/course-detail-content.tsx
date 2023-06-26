698a06a1fac8db8877b24c3444a0c695c9971c94
Novian Andika <hello@fleetime.my.id>
2023-06-27 06:30:24 +0700

    🎨 style(course-detail-content.tsx): improve styling of tabs and tab triggers The styling of the tabs and tab triggers has been improved to make them more visually appealing and consistent with the rest of the application. The tabs now have a border and a background color, and the tab triggers have a more prominent and consistent font size and weight. The tab triggers also have a transition effect when they are clicked.

--- components/app/course/detail/course-detail-content.tsx
+++ components/app/course/detail/course-detail-content.tsx
@@ -37,12 +37,29 @@ export function CourseDetailContent(props: {
           height={720}
         /> */}
         <YoutubePlayer videoId="fqQ1Xum8uNI" />
-        <Tabs defaultValue="description" className="hidden md:block">
-          <TabsList className="grid w-full grid-cols-3">
-            <TabsTrigger value="description">Deskripsi</TabsTrigger>
-            <TabsTrigger value="time">Waktu</TabsTrigger>
-            <TabsTrigger value="users">Murid</TabsTrigger>
-          </TabsList>
+        <Tabs defaultValue="description" className="relative mr-auto w-full">
+          <div className="flex items-center justify-between pb-3">
+            <TabsList className="w-full justify-start rounded-none border-b bg-transparent p-0">
+              <TabsTrigger
+                value="description"
+                className="relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+              >
+                Deskripsi
+              </TabsTrigger>
+              <TabsTrigger
+                value="time"
+                className="relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+              >
+                Waktu
+              </TabsTrigger>
+              <TabsTrigger
+                value="users"
+                className="relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+              >
+                Murid
+              </TabsTrigger>
+            </TabsList>
+          </div>
           <TabsContent value="description">
             <Card>
               <CardHeader>
