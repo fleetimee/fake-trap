d66f705fc5129ed558736adfd35f9e97608680d1
Novian Andika <novian.andika@students.amikom.ac.id>
2023-06-27 11:28:23 +0700

    🐛 fix(page.tsx): import Alert, AlertDescription, and AlertTitle components to fix reference error ✨ feat(page.tsx): add DetailSidebarKnowledge component to display knowledge information on course detail page 🐛 fix(thunderCollection.json): update knowledge id in Get Knowledge by Id request 🐛 fix(thunderclient.json): update course id in Get Course By ID request

--- app/(dashboard)/dashboard/course/[detail]/page.tsx
+++ app/(dashboard)/dashboard/course/[detail]/page.tsx
@@ -1,8 +1,12 @@
 import { Metadata } from "next"
+import { RocketIcon } from "@radix-ui/react-icons"
 
 import { getCourseById } from "@/lib/fetcher/course/course-fetcher"
+import { getKnowledgeByid } from "@/lib/fetcher/knowledge/knowledge-fetcher"
 import { getUser } from "@/lib/fetcher/user/user-fetcher"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { CourseDetailContent } from "@/components/app/course/detail/course-detail-content"
+import DetailSidebarKnowledge from "@/components/app/knowledge/detail-sidebar-knowledge"
 import { DashboardShell } from "@/components/shell"
 
 type Props = {
@@ -32,10 +36,29 @@ export default async function DetailCourse({
     userList,
   ])
 
+  const courseKnowledgeResp = await getKnowledgeByid(
+    courseDataResp.data.id_knowledge
+  )
+
+  console.log(courseKnowledgeResp)
+
   return (
     <DashboardShell>
+      <div className="flex flex-row gap-4 px-2">
+        <Alert className="basis-full">
+          <RocketIcon className="h-4 w-4" />
+          <AlertTitle>Informasi!</AlertTitle>
+          <AlertDescription>
+            Kursus ini berdasarkan pada pengetahuan{" "}
+            <span className="font-bold">
+              {courseKnowledgeResp.data.knowledge_title}
+            </span>
+          </AlertDescription>
+        </Alert>
+      </div>
       <div className="flex h-auto flex-col gap-4 px-2 lg:flex-row">
         <CourseDetailContent data={courseDataResp} user={userDataResp} />
+        <DetailSidebarKnowledge data={courseKnowledgeResp.data} />
       </div>
     </DashboardShell>
   )
