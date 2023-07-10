1ff62b3479b8900967a5cf31218cb23439106f49
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-10 14:04:48 +0700

    🔨 refactor(category-operations.tsx): remove unnecessary line break in CategoryOperations component

--- components/app/category/category-operations.tsx
+++ components/app/category/category-operations.tsx
@@ -127,7 +127,6 @@ export function CategoryOperations(props: { kategori: DataCategory }) {
 
         router.refresh()
         form.reset()
-
         setOpenEditCategorySheet(false)
       } else {
         toast({
