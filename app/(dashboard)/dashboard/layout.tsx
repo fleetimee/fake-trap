3ea16aaa76256447e11b623092d3eed898efe3f2
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-13 14:32:10 +0700

    🔧 fix(layout.tsx): remove unused import "use client" to improve code cleanliness and readability 🔧 fix(layout.tsx): remove unused import { SiteFooter } to improve code cleanliness and readability 🔧 fix(layout.tsx): remove unused import UserAccountNav to improve code cleanliness and readability 🔧 fix(layout.tsx): remove unused import { getCurrentUser } to improve code cleanliness and readability ✨ feat(layout.tsx): add import { SiteHeader } to use in the layout component ✨ feat(layout.tsx): add async keyword to DashboardLayout function to enable asynchronous behavior ✨ feat(layout.tsx): add user prop to SiteHeader component to display user information in the header

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,7 @@
-"use client"
-
 import { dashboardConfig } from "@/config/dashboard"
+import { getCurrentUser } from "@/lib/session"
 import { SiteFooter } from "@/components/layouts/site-footer"
+import { SiteHeader } from "@/components/layouts/site-header"
 import { MainNav } from "@/components/main-nav"
 import { DashboardNav } from "@/components/nav"
 import UserAccountNav from "@/components/user-account-nav"
@@ -10,21 +10,14 @@ interface DashboardLayoutProps {
   children: React.ReactNode
 }
 
-export default function DashboardLayout({ children }: DashboardLayoutProps) {
+export default async function DashboardLayout({
+  children,
+}: DashboardLayoutProps) {
+  const user = await getCurrentUser()
+
   return (
     <div className="flex min-h-screen flex-col space-y-6 ">
-      <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} />
-          <UserAccountNav
-            user={{
-              name: "User",
-              image: "https://avatars.githubusercontent.com/u/45744788?v=4",
-              email: "zane.227@gmail.com",
-            }}
-          />
-        </div>
-      </header>
+      <SiteHeader user={user} />
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
         <aside className="hidden w-[200px] flex-col md:flex">
           <DashboardNav items={dashboardConfig.sidebarNav} />
