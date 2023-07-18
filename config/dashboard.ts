b9faffdea53fc78806ea9befaeedc2ecdb40b4b2
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-18 15:09:06 +0700

    🚀 feat(dashboard.ts): add empty items array to each dashboard menu item for future expansion

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -26,36 +26,43 @@ export const dashboardConfig: DashboardConfig = {
       title: "Dashboard",
       href: "/dashboard",
       icon: "dashboard",
+      items: [],
     },
     {
       title: "User",
       href: "/dashboard/user",
       icon: "user",
+      items: [],
     },
     {
       title: "Kategori",
       href: "/dashboard/category",
       icon: "category",
+      items: [],
     },
     {
       title: "Quiz",
       href: "/dashboard/quiz",
       icon: "quiz",
+      items: [],
     },
     {
       title: "Pengetahuan",
       href: "/dashboard/knowledge",
       icon: "knowledge",
+      items: [],
     },
     {
       title: "Kursus",
       href: "/dashboard/course",
       icon: "course",
+      items: [],
     },
     {
       title: "Settings",
       href: "/dashboard/settings",
       icon: "settings",
+      items: [],
     },
   ],
 }
