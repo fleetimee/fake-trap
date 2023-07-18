a649986003a96ce4e7a3da93ef60c3331bc00e53
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-18 15:03:58 +0700

    🔧 fix(site-header.tsx): fix import statements for siteConfig and MobileNav components ✨ feat(site-header.tsx): add support for mobile navigation menu using MobileNav component 🔧 fix(site-header.tsx): fix import statements for siteConfig and MobileNav components ✨ feat(site-header.tsx): add support for mobile navigation menu using MobileNav component

--- components/layouts/site-header.tsx
+++ components/layouts/site-header.tsx
@@ -4,9 +4,11 @@ import Link from "next/link"
 import { signOut } from "next-auth/react"
 
 import { dashboardConfig } from "@/config/dashboard"
+import { siteConfig } from "@/config/site"
 import { extractToken } from "@/lib/utils"
 
 import { MainNav } from "../main-nav"
+import { MobileNav } from "../mobile-nav"
 import { Avatar, AvatarFallback } from "../ui/avatar"
 import { buttonVariants } from "../ui/button"
 import {
@@ -35,72 +37,80 @@ export function SiteHeader(props: {
 
     return (
       <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} />
-          {props.user ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar className="h-8 w-8">
-                  <AvatarFallback>{initial}</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <div className="flex items-center justify-start gap-2 p-2">
-                  <div className="flex flex-col space-y-1 leading-none">
-                    {userExtracted.username && (
-                      <p className="font-medium">{userExtracted.username}</p>
-                    )}
-                    {userExtracted.email && (
-                      <p className="w-[200px] truncate text-sm text-muted-foreground">
-                        {userExtracted.email}
-                      </p>
-                    )}
-                  </div>
-                </div>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  className="cursor-pointer"
-                  onSelect={(event) => {
-                    event.preventDefault()
-                    signOut({
-                      callbackUrl: `${window.location.origin}`,
-                    }).then(() => {
-                      localStorage.clear()
-                      sessionStorage.clear()
+        <div className="container flex h-16 items-center">
+          <MainNav items={siteConfig.mainNav} />
+          <MobileNav
+            mainNavItems={siteConfig.mainNav}
+            sidebarNavItems={dashboardConfig.sidebarNav}
+          />
+          <div className="flex flex-1 items-center justify-end space-x-4">
+            <nav className="flex items-center space-x-2"></nav>
 
-                      document.cookie.split(";").forEach((c) => {
-                        document.cookie = c
-                          .replace(/^ +/, "")
-                          .replace(
-                            /=.*/,
-                            `=;expires=${new Date().toUTCString()};path=/`
-                          )
+            {props.user ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar className="h-8 w-8">
+                    <AvatarFallback>{initial}</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <div className="flex items-center justify-start gap-2 p-2">
+                    <div className="flex flex-col space-y-1 leading-none">
+                      {userExtracted.username && (
+                        <p className="font-medium">{userExtracted.username}</p>
+                      )}
+                      {userExtracted.email && (
+                        <p className="w-[200px] truncate text-sm text-muted-foreground">
+                          {userExtracted.email}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    className="cursor-pointer"
+                    onSelect={(event) => {
+                      event.preventDefault()
+                      signOut({
+                        callbackUrl: `${window.location.origin}`,
+                      }).then(() => {
+                        localStorage.clear()
+                        sessionStorage.clear()
+
+                        document.cookie.split(";").forEach((c) => {
+                          document.cookie = c
+                            .replace(/^ +/, "")
+                            .replace(
+                              /=.*/,
+                              `=;expires=${new Date().toUTCString()};path=/`
+                            )
+                        })
                       })
-                    })
-                  }}
-                >
-                  Sign out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : (
-            <Link
-              href="/login"
-              target="_blank"
-              rel="noopener noreferrer"
-              replace
-              prefetch={false}
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                })}
+                    }}
+                  >
+                    Sign out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : (
+              <Link
+                href="/login"
+                target="_blank"
+                rel="noopener noreferrer"
+                replace
+                prefetch={false}
               >
-                Sign In
-                <span className="sr-only">Masuk</span>
-              </div>
-            </Link>
-          )}
+                <div
+                  className={buttonVariants({
+                    size: "lg",
+                  })}
+                >
+                  Sign In
+                  <span className="sr-only">Masuk</span>
+                </div>
+              </Link>
+            )}
+          </div>
         </div>
       </header>
     )
@@ -108,9 +118,11 @@ export function SiteHeader(props: {
     return (
       <header className="sticky top-0 z-40 border-b bg-background">
         <div className="container flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} />
-          {/* <UserAccountNav user={props.user} /> */}
-
+          <MainNav items={siteConfig.mainNav} />
+          <MobileNav
+            mainNavItems={siteConfig.mainNav}
+            sidebarNavItems={dashboardConfig.sidebarNav}
+          />
           {props.user ? (
             <DropdownMenu>
               <DropdownMenuTrigger>
