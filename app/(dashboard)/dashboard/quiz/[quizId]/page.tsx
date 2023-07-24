8d5297ff417e947beffe4475d261a2e759403b5a
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-24 15:37:52 +0700

    🐛 fix(page.tsx): fix typo in import statement for convertDatetoString function ✨ feat(page.tsx): add new imports for getUsersQuizCountById and Card components to enhance functionality and UI design 🔀 merge(page.tsx): merge changes from remote branch to local branch to incorporate new features and bug fixes

--- app/(dashboard)/dashboard/quiz/[quizId]/page.tsx
+++ app/(dashboard)/dashboard/quiz/[quizId]/page.tsx
@@ -2,8 +2,10 @@ import { Metadata } from "next"
 import { redirect } from "next/navigation"
 
 import { authOptions } from "@/lib/auth"
-import { getQuizById } from "@/lib/datasource"
+import { getQuizById, getUsersQuizCountById } from "@/lib/datasource"
 import { getCurrentUser } from "@/lib/session"
+import { convertDatetoString } from "@/lib/utils"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { DashboardShell } from "@/components/shell"
 
 type Props = {
@@ -32,10 +34,191 @@ export default async function QuizDetailPage({ params }: Props) {
     redirect(authOptions?.pages?.signIn || "/login")
   }
 
-  const detailQuizData = await getQuizById({
+  const detailQuizData = getQuizById({
     id: params.quizId,
     token: user?.token,
   })
 
-  return <p>Penis</p>
+  const detailQuizUsersCount = getUsersQuizCountById({
+    id: params.quizId,
+    token: user?.token,
+  })
+
+  const [detailQuizDataResp, detailQuizUsersCountResp] = await Promise.all([
+    detailQuizData,
+    detailQuizUsersCount,
+  ])
+
+  const isAlreadyHaveQuiz = detailQuizDataResp.data.questions ? true : false
+
+  const quizUsersCount = detailQuizUsersCountResp.data.count
+
+  const quizType =
+    detailQuizDataResp.data.quiz_type == 1 ? "Pre Test" : "Post Test"
+
+  return (
+    <div className="space-y-4">
+      <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-3">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Sudah Ada Pertanyaan ?
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {isAlreadyHaveQuiz ? "Sudah" : "Belum"}
+            </div>
+            <p className="text-xs text-muted-foreground">
+              {isAlreadyHaveQuiz
+                ? "Kamu sudah membuat pertanyaan untuk kuis ini"
+                : "Kamu belum membuat pertanyaan untuk kuis ini"}
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Mengikuti Quiz Ini
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{quizUsersCount}</div>
+            <p className="text-xs text-muted-foreground">Peserta</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Tipe Kuis</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{quizType}</div>
+            <p className="text-xs text-muted-foreground">..</p>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div className="grid grid-flow-row grid-rows-3 gap-4 md:grid-cols-1 lg:grid-cols-2">
+        <Card className="row-span-3 h-auto">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Deskripsi</CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <p className="text-xs text-muted-foreground">
+              {detailQuizDataResp.data.quiz_desc}
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="row-span-1 h-auto">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Tanggal Pembuatan
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              {convertDatetoString(
+                detailQuizDataResp.data.created_at.toString()
+              )}
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="row-span-2 h-auto">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">
+              Tujuan Quiz Ini
+            </CardTitle>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="h-4 w-4 text-muted-foreground"
+            >
+              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+              <circle cx="9" cy="7" r="4" />
+              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+          </CardHeader>
+          <CardContent>
+            <p className="text-xs text-muted-foreground">
+              {detailQuizDataResp.data.quiz_desc}
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
