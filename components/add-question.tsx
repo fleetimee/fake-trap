7abce1a12c52db131cbebe72fdd6420f40b01462
Novian Andika <hello@fleetime.my.id>
2023-07-30 20:09:21 +0700

    🐛 fix(add-question.tsx): add validation for nonempty question_text and answer_text fields ✨ feat(add-question.tsx): add form submission logic to check for correct answer and display success or error toast messages The fix ensures that the question_text and answer_text fields cannot be empty by adding validation using the `.nonempty()` method from the `zod` library. The feat adds form submission logic to check if there is at least one correct answer, if the correct answer is only one, and if there are at least two answer choices. It also displays success or error toast messages based on the validation results.

--- components/add-question.tsx
+++ components/add-question.tsx
@@ -7,16 +7,23 @@ import { z } from "zod"
 import { Button } from "./ui/button"
 import { Card } from "./ui/card"
 import { Checkbox } from "./ui/checkbox"
-import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form"
 import { Input } from "./ui/input"
+import { toast } from "./ui/use-toast"
 
 const formSchema = z.object({
   id_quiz: z.number(),
-  question_text: z.string(),
+  question_text: z.string().nonempty(),
   answers: z.array(
     z.object({
-      id: z.number(),
-      answer_text: z.string(),
+      answer_text: z.string().nonempty(),
       is_correct: z.boolean().default(false).optional(),
     })
   ),
@@ -30,7 +37,6 @@ export function QuestionForm() {
       question_text: "",
       answers: [
         {
-          id: 1,
           answer_text: "",
           is_correct: false,
         },
@@ -43,21 +49,64 @@ export function QuestionForm() {
     control: form.control,
   })
 
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    // check if there is any correct answer
+    const isCorrectAnswer = form.getValues("answers").some((answer) => {
+      return answer.is_correct
+    })
+
+    // check if the correct answer is only one
+    const correctAnswer = form.getValues("answers").filter((answer) => {
+      return answer.is_correct
+    })
+
+    if (form.getValues("answers").length < 2) {
+      return toast({
+        title: "Error",
+        description: "Jawaban minimal 2 pilihan",
+        variant: "destructive",
+      })
+    }
+
+    if (!isCorrectAnswer) {
+      return toast({
+        title: "Error",
+        description: "Jawaban benar harus ada",
+        variant: "destructive",
+      })
+    }
+
+    if (correctAnswer.length > 1) {
+      return toast({
+        title: "Error",
+        description: "Jawaban benar hanya boleh satu",
+        variant: "destructive",
+      })
+    }
+
+    return toast({
+      title: "Success",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
   return (
     <Card className="flex flex-col gap-8 p-5">
-      <div className="flex flex-row items-center justify-between">
-        <h1 className="font-heading font-semibold">Test Layouting</h1>
-
-        <Button>Push Pertanyaan</Button>
-      </div>
-
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit((data) => {
-            console.log(data)
-          })}
+          onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col space-y-8"
         >
+          <div className="flex flex-row items-center justify-between">
+            <h1 className="font-heading font-semibold">Test Layouting</h1>
+
+            <Button type="submit">Push Pertanyaan</Button>
+          </div>
+
           <FormField
             control={form.control}
             name="question_text"
@@ -67,6 +116,7 @@ export function QuestionForm() {
                 <FormControl>
                   <Input placeholder="Masukkan pertanyaan" {...field} />
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
@@ -80,9 +130,13 @@ export function QuestionForm() {
                   render={({ field }) => (
                     <FormItem>
                       <FormLabel>Jawaban {index + 1}</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Masukkan jawaban" {...field} />
-                      </FormControl>
+                      <div className="flex flex-row items-center space-x-3">
+                        <FormControl>
+                          <Input placeholder="Masukkan jawaban" {...field} />
+                        </FormControl>
+                        <Button onClick={() => remove(index)}>Remove</Button>
+                      </div>
+                      <FormMessage />
                     </FormItem>
                   )}
                 />
@@ -102,16 +156,14 @@ export function QuestionForm() {
                   )}
                 />
               </div>
-
-              <Button onClick={() => remove(index)}>Remove</Button>
             </div>
           ))}
         </form>
       </Form>
 
-      <Button
-        onClick={() => append({ id: 1, answer_text: "", is_correct: false })}
-      />
+      <Button onClick={() => append({ answer_text: "", is_correct: false })}>
+        Tambah Jawaban
+      </Button>
     </Card>
   )
 }
