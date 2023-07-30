2bc67f9f5d59b086badee6ed71feb474e25673dc
Novian Andika <hello@fleetime.my.id>
2023-07-30 20:09:00 +0700

    🎨 style(page.tsx): refactor layout and remove unused code for better readability and maintainability The layout of the page has been refactored to use a grid with two columns instead of one. The unused QuizFormInner component has been removed to improve code cleanliness. Additionally, the Card component from the UI library is now used instead of the Button component. This change improves the overall readability and maintainability of the code.

--- app/(dashboard)/dashboard/quiz/[quizId]/soal/page.tsx
+++ app/(dashboard)/dashboard/quiz/[quizId]/soal/page.tsx
@@ -1,24 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
 import { QuestionForm } from "@/components/add-question"
 
 export default function HasilPage() {
-  const QuizFormInner = () => {
-    return (
-      <div className="flex flex-col gap-8 p-5">
-        <div className="flex flex-row items-center justify-between">
-          <h1 className="font-heading font-semibold">Test Layouting</h1>
-
-          <Button>Push Pertanyaan</Button>
-        </div>
-
-        <p>This should be on bottom</p>
-      </div>
-    )
-  }
-
   return (
-    <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+    <div className="grid grid-cols-2  gap-4 lg:grid-cols-2">
       <QuestionForm />
+
+      <Card className="flex flex-col gap-8 p-5">Penis</Card>
     </div>
   )
 }
