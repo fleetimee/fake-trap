8cf84d8acb6c5c20695732f084f25d9b2113873d
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-31 11:29:36 +0700

    ✨ feat(quiz-lottie-animation.tsx): add new component `LottieAnimationQuiz` to display a lottie animation for quizzes

---
+++ components/quiz-lottie-animation.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import AnimationData from "@/public/lottie/quiz_temp.json"
+import Lottie from "lottie-react"
+
+export function LottieAnimationQuiz() {
+  return <Lottie animationData={AnimationData} />
+}
