a3075809a6f0fc62c80ccf8e5817119ec3f7b8d0
Novian Andika <novian.andika@students.amikom.ac.id>
2023-07-31 11:27:45 +0700

    🔧 fix(add-question-shell.tsx): import Checkbox component to fix missing import error ✨ feat(add-question-shell.tsx): add conditional rendering for displaying quiz results or a message to add questions 🔧 fix(add-question-shell.tsx): fix typo in comment

--- components/add-question-shell.tsx
+++ components/add-question-shell.tsx
@@ -5,8 +5,10 @@ import { useAutoAnimate } from "@formkit/auto-animate/react"
 import { z } from "zod"
 
 import { QuestionForm } from "./add-question"
+import { LottieAnimationQuiz } from "./quiz-lottie-animation"
 import { Button } from "./ui/button"
 import { Card } from "./ui/card"
+import { Checkbox } from "./ui/checkbox"
 
 export function SoalShell() {
   const [parent, enableAnimations] = useAutoAnimate()
@@ -34,39 +36,63 @@ export function SoalShell() {
     <>
       <QuestionForm setQuizzes={setQuizzes} />
 
-      <Card className="flex flex-col gap-8 p-5" ref={parent}>
-        <div className="flex items-center justify-between">
-          <h1 className="font-heading font-semibold">Soal</h1>
-          <Button>Submit Quiz</Button>
-        </div>
-
-        {quizzes.map((quiz, index) => (
-          <div key={index}>
-            <div className="flex items-center justify-between">
-              <p className="space-y-4 pb-4 text-sm font-medium leading-none">
-                {index + 1}. {quiz.question_text}
-              </p>
-              <Button
-                variant="destructive"
-                size="sm"
-                onClick={() => deleteQuestion(index)}
-              >
-                Hapus
-              </Button>
-            </div>
-            <ul className="grid list-inside list-disc grid-cols-2 gap-4">
-              {quiz.answers.map((answer, index) => (
-                <li key={index} className="flex items-center gap-3">
-                  <input type="checkbox" checked={answer.is_correct} />
-                  <p className="text-sm font-medium leading-none">
-                    {answer.answer_text}
-                  </p>
-                </li>
-              ))}
-            </ul>
+      {quizzes.length > 0 && (
+        <Card className="flex flex-col gap-8 p-5" ref={parent}>
+          <div className="flex items-center justify-between">
+            <h1 className="font-heading font-semibold">Hasil Soal</h1>
+            <Button
+              onClick={() => {
+                console.log(quizzes)
+              }}
+            >
+              Submit Quiz
+            </Button>
           </div>
-        ))}
-      </Card>
+
+          {quizzes.map((quiz, index) => (
+            <div key={index}>
+              <div className="flex items-center justify-between">
+                <p className="space-y-4 pb-4 text-sm font-semibold leading-none">
+                  {index + 1}. {quiz.question_text}
+                </p>
+                <Button
+                  variant="destructive"
+                  size="sm"
+                  onClick={() => deleteQuestion(index)}
+                >
+                  Hapus
+                </Button>
+              </div>
+              <ul className="grid list-inside list-disc grid-cols-2 gap-4">
+                {quiz.answers.map((answer, index) => (
+                  <li key={index} className="flex items-center gap-3">
+                    {/* <input type="checkbox" checked={answer.is_correct} /> */}
+
+                    <Checkbox checked={answer.is_correct} />
+
+                    <p className="text-sm font-medium leading-none">
+                      {answer.answer_text}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </Card>
+      )}
+
+      {quizzes.length == 0 && (
+        <Card
+          className="flex flex-col items-center justify-center gap-8 p-5"
+          ref={parent}
+        >
+          <LottieAnimationQuiz />
+
+          <p className="font-medium leading-none">
+            Tambahkan pertanyaan untuk melihat hasil soal
+          </p>
+        </Card>
+      )}
     </>
   )
 }
