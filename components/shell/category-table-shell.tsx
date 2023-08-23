da8636f76a410f31ff08db7a156040a0b3296c99
Novian Andika <novian.andika@students.amikom.ac.id>
2023-08-23 11:07:44 +0700

    feat: preparing

---
+++ components/shell/category-table-shell.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { DotsHorizontalIcon } from "@radix-ui/react-icons"
+import { type ColumnDef } from "@tanstack/react-table"
+
+import { CategoryListResData } from "@/types/category/res"
+import { convertDatetoString } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { DataTable } from "@/components/data-table/data-table"
+import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header"
+
+import { CategoryOperations } from "../app/category/category-operations"
+
+interface CategoryTableShellProps {
+  data: CategoryListResData[]
+  pageCount: number
+}
+
+export function CategoryTableShell({
+  data,
+  pageCount,
+}: CategoryTableShellProps) {
+  const [isPending, startTransition] = React.useTransition()
+  const [selectedRowIds, setSelectedRowIds] = React.useState<number[]>([])
+
+  const columns = React.useMemo<ColumnDef<CategoryListResData>[]>(
+    () => [
+      {
+        accessorKey: "category_name",
+        header: ({ column }) => (
+          <DataTableColumnHeader column={column} title="Nama" />
+        ),
+        enableSorting: true,
+        enableHiding: true,
+      },
+      {
+        accessorKey: "created_at",
+        header: "Dibuat pada",
+        cell: ({ row }) => {
+          convertDatetoString(row.original.created_at.toString())
+
+          return <>{convertDatetoString(row.original.created_at.toString())}</>
+        },
+      },
+      {
+        accessorKey: "updated_at",
+        header: "Diubah pada",
+        cell: ({ row }) => {
+          convertDatetoString(row.original.updated_at.toString())
+
+          return <>{convertDatetoString(row.original.updated_at.toString())}</>
+        },
+      },
+      {
+        header: "Aksi",
+        id: "actions",
+        cell: ({ row }) => {
+          const kategori = row.original
+
+          return <CategoryOperations kategori={kategori} />
+        },
+      },
+    ],
+    [data, isPending]
+  )
+
+  return <DataTable columns={columns} data={data} pageCount={pageCount} />
+}
