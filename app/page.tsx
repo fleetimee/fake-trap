965684d729f2f5694977865dcef44712e0b40c48
Novian Andika <novian.andika@students.amikom.ac.id>
2023-09-15 16:30:38 +0700

    initial changes
    Signed-off-by: Novian Andika <novian.andika@students.amikom.ac.id>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import Balancer from "react-wrap-balancer"
 
 import { getCurrentUser } from "@/lib/session"
-import { buttonVariants } from "@/components/ui/button"
 import { SiteFooter } from "@/components/layouts/site-footer"
 import { SiteHeader } from "@/components/layouts/site-header"
 import { LottieAnimation } from "@/components/lottie-animation"
+import { buttonVariants } from "@/components/ui/button"
 
 export const metadata = {
   title: "BPD E-learning: Pelajari apa saja, kapan saja, di mana saja",
@@ -18,19 +19,28 @@ export default async function IndexPage() {
     <>
       <div className="relative flex min-h-screen flex-col">
         <SiteHeader user={user} />
-        <div className="container flex flex-col justify-between py-24 md:flex-row">
-          <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-            <div className="flex max-w-[980px] flex-col items-start gap-2">
-              <h1 className="font-heading text-3xl font-extrabold leading-tight tracking-tight md:text-4xl">
-                E-learning BPD DIY memberikan anda{" "}
-                <br className="hidden sm:inline" />
-                akses ke ribuan kursus online
-              </h1>
-              <p className="max-w-[700px] text-lg text-muted-foreground">
-                Pelajari apa saja, kapan saja, di mana saja
-              </p>
-              <pre className="text-sm text-muted-foreground"></pre>
-            </div>
+        <div className="gap-12">
+          <section
+            id="hero"
+            aria-labelledby="hero-heading"
+            className="mx-auto flex w-full max-w-[64rem] flex-col items-center justify-center gap-4 bg-background pb-8 pt-6 text-center md:pb-12 md:pt-10 lg:py-28"
+          >
+            <h1
+              className="animate-fade-up bg-gradient-to-br from-foreground to-muted-foreground bg-clip-text text-center text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl/[5rem]"
+              style={{ animationDelay: "0.20s", animationFillMode: "forwards" }}
+            >
+              <Balancer>Pelajari apa saja, kapan saja, di mana saja</Balancer>
+            </h1>
+            <p
+              className="animate-fade-up mt-6 text-center text-muted-foreground/80 opacity-0 md:text-xl"
+              style={{ animationDelay: "0.30s", animationFillMode: "forwards" }}
+            >
+              <Balancer>
+                Acme Corp is a Next.js starter kit that includes everything you
+                need to build a modern web application. Mobile application
+                preconfigured, ready to go.
+              </Balancer>
+            </p>
             <div className="flex gap-4">
               <Link
                 href={"/intro/"}
@@ -52,7 +62,36 @@ export default async function IndexPage() {
               </Link>
             </div>
           </section>
-          <LottieAnimation />
+
+          <section className="space-y-6 py-6 md:pt-10 lg:pt-24">
+            <div className="animate-fade-up mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+              <h2 className="pt-4 text-center text-3xl font-bold md:text-4xl">
+                What&apos;s included?
+              </h2>
+
+              <p className="pb-8 pt-4 text-center text-lg">
+                <Balancer>
+                  This repo comes fully stacked with everything you need for
+                  your enterprise startup. Stop worrying about boilerplate
+                  integrations and start building your product today!
+                </Balancer>
+              </p>
+            </div>
+
+            {/*<div className="grid grid-cols-1 gap-5 md:grid-cols-3">*/}
+            {/*  {marketingFeatures.map((feature) => (*/}
+            {/*    <Card key={feature.title} className={cn("p-2")}>*/}
+            {/*      <CardHeader>{feature.icon}</CardHeader>*/}
+            {/*      <CardContent className="space-y-2">*/}
+            {/*        <CardTitle>{feature.title}</CardTitle>*/}
+            {/*        <CardDescription className="mt-2">*/}
+            {/*          {feature.body}*/}
+            {/*        </CardDescription>*/}
+            {/*      </CardContent>*/}
+            {/*    </Card>*/}
+            {/*  ))}*/}
+            {/*</div>*/}
+          </section>
         </div>
       </div>
       <SiteFooter className="border-t" />
