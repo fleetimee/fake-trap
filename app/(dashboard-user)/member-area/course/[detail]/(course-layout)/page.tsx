023a7104d0eec54e0ff7bab90dc7f6cd35b4f469
Novian Andika <novian.andika@students.amikom.ac.id>
2023-10-23 16:58:15 +0700

    feat: pointless

--- app/(dashboard-user)/member-area/course/[detail]/(course-layout)/page.tsx
+++ app/(dashboard-user)/member-area/course/[detail]/(course-layout)/page.tsx
@@ -1,8 +1,33 @@
+import Image from "next/image"
 import { redirect } from "next/navigation"
 
+import { CourseOneRes } from "@/types/course/res"
 import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
 import { MotionDiv } from "@/components/framer-wrapper"
+
+interface GetOneCourseProps {
+  token: string | undefined
+  idKnowledge: string
+}
+
+async function getOneCourse({
+  token,
+  idKnowledge,
+}: GetOneCourseProps): Promise<CourseOneRes> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/secure/course/${idKnowledge}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
+
+  return await res.json()
+}
 
 interface MemberCourseDetailPageProps {
   params: {
@@ -10,19 +35,27 @@ interface MemberCourseDetailPageProps {
   }
 }
 
-export default async function MemberCourseDetailPage({}: MemberCourseDetailPageProps) {
+export default async function MemberCourseDetailPage({
+  params,
+}: MemberCourseDetailPageProps) {
   const user = await getCurrentUser()
 
   if (!user) {
     redirect(authOptions?.pages?.signIn || "/login")
   }
 
+  const courseOneRes = await getOneCourse({
+    token: user?.token,
+    idKnowledge: params.detail,
+  })
+
   return (
-    <MotionDiv
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 0.5 } }}
-    >
-      <p className="text-2xl font-bold text-gray-800">Course Detail</p>
-    </MotionDiv>
+    <Image
+      src={courseOneRes.data.image}
+      alt={courseOneRes.data.course_name}
+      className="aspect-video rounded-lg object-cover shadow-md grayscale hover:grayscale-0"
+      width={1280}
+      height={720}
+    />
   )
 }
