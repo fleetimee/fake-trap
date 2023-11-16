ff43c556b26c71297b845ae3788ef5b347a1e657
Novian Andika <novian.andika@students.amikom.ac.id>
2023-11-16 08:06:50 +0700

    Refactored code to improve performance and readability.

---
+++ app/(dash-supervisor-lms)/supervisor-lms/approval/page.tsx
@@ -0,0 +1,3 @@
+export default function SupervisorLmsApprovalPage() {
+  return <p>SupervisorLmsApprovalPage</p>
+}
---
+++ app/(dash-supervisor-pemateri-divisi)/supervisor-pemateri-divisi/setting/page.tsx
@@ -0,0 +1,3 @@
+export default function SupervisorPemateriDivisiSettingPage() {
+  return <p>SupervisorPemateriDivisiSettingPage</p>
+}
