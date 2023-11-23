712731574fcb0358ff685494d39ec864f584589f
Novian Andika <novian.andika@students.amikom.ac.id>
2023-11-23 11:42:23 +0700

    Refactor login page layout and add forgot password link

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react"
-import { Command } from "lucide-react"
+import Link from "next/link"
 
 import { UserAuthForm } from "@/components/user-auth-form"
 
@@ -10,44 +10,26 @@ export const metadata = {
 
 export default async function LoginPage() {
   return (
-    <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:h-full lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div
-          className="absolute inset-0 bg-cover"
-          style={{
-            backgroundImage: "url(/images/bg-login.jpg)",
-          }}
-        />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <Command className="mr-2 h-6 w-6" /> E Learning BPD - Login
-        </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;Elearning BPD DIY adalah platform yang inovatif dan
-              berkualitas tinggi dalam memberikan pendidikan online yang
-              memungkinkan akses yang mudah, interaktif, dan berkelanjutan bagi
-              semua peserta didik!&rdquo;
-            </p>
-            <footer className="text-sm">Admin</footer>
-          </blockquote>
-        </div>
+    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
+      <div className="flex flex-col space-y-2 text-center">
+        <h1 className="font-heading text-2xl font-semibold tracking-tight">
+          Masuk ke E Learning BPD
+        </h1>
+        <p className="text-sm text-muted-foreground">
+          Masukkan username dan password anda untuk mengakses E Learning BPD
+        </p>
       </div>
-      <div className="flex h-full flex-col items-center justify-center lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] ">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="font-heading text-2xl font-semibold tracking-tight">
-              Masuk ke E Learning BPD
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Masukkan username dan password anda untuk mengakses E Learning BPD
-            </p>
-          </div>
-          <Suspense fallback={<div>Loading...</div>}>
-            <UserAuthForm />
-          </Suspense>
-        </div>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserAuthForm />
+      </Suspense>
+      <Link href="/login/forgot-password">
+        {" "}
+        {/* Step 2: Add a new Link element */}
+        <p className="text-end text-sm text-muted-foreground hover:text-blue-500 hover:underline">
+          Lupa password ?
+        </p>{" "}
+        {/* Step 3 and 4: Add the text and style the Link element */}
+      </Link>
     </div>
   )
 }
