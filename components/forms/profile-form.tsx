1e8dea0bf6b5e234611a40328262ed049b2c3d60
Novian Andika <novian.andika@students.amikom.ac.id>
2023-11-23 11:43:18 +0700

    Refactor profile form component

--- components/forms/profile-form.tsx
+++ components/forms/profile-form.tsx
@@ -1,12 +1,17 @@
 "use client"
 
-import Link from "next/link"
+import { useTransition } from "react"
+import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useFieldArray, useForm } from "react-hook-form"
+import { useSession } from "next-auth/react"
+import { useForm } from "react-hook-form"
+import { toast as sonnerToast } from "sonner"
 import * as z from "zod"
 
-import { cn } from "@/lib/utils"
+import { UserOneResData } from "@/types/user/res"
+import { profileSchema } from "@/lib/validations/profile"
 
+import { Icons } from "../icons"
 import { Button } from "../ui/button"
 import {
   Form,
@@ -18,71 +23,52 @@ import {
   FormMessage,
 } from "../ui/form"
 import { Input } from "../ui/input"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select"
-import { Textarea } from "../ui/textarea"
-import { toast } from "../ui/use-toast"
 
-const profileFormSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: "Username must be at least 2 characters.",
-    })
-    .max(30, {
-      message: "Username must not be longer than 30 characters.",
-    }),
-  email: z
-    .string({
-      required_error: "Please select an email to display.",
-    })
-    .email(),
-  bio: z.string().max(160).min(4),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
-    )
-    .optional(),
-})
+type Inputs = z.infer<typeof profileSchema>
 
-type ProfileFormValues = z.infer<typeof profileFormSchema>
-
-// This can come from your database or API.
-const defaultValues: Partial<ProfileFormValues> = {
-  bio: "I own a computer.",
-  urls: [
-    { value: "https://shadcn.com" },
-    { value: "http://twitter.com/shadcn" },
-  ],
+interface ProfileFormProps {
+  person: UserOneResData
 }
 
-export function ProfileForm() {
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(profileFormSchema),
-    defaultValues,
-    mode: "onChange",
+export function ProfileForm({ person }: ProfileFormProps) {
+  const { data: session } = useSession()
+
+  const router = useRouter()
+
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<Inputs>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      username: person?.username,
+      email: person?.email,
+    },
   })
 
-  const { fields, append } = useFieldArray({
-    name: "urls",
-    control: form.control,
-  })
+  async function onSubmit(data: Inputs) {
+    startTransition(async () => {
+      try {
+        const url = `${process.env.NEXT_PUBLIC_BASE_URL}/secure/users/${person.uuid}`
 
-  function onSubmit(data: ProfileFormValues) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
+        const response = await fetch(url, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user.token}`,
+          },
+          body: JSON.stringify(data),
+        })
+
+        if (response.ok) {
+          sonnerToast.success("Berhasil mengubah profile")
+
+          router.refresh()
+        } else {
+          sonnerToast.error("Gagal mengubah profile")
+        }
+      } catch (error) {
+        sonnerToast.error("Gagal mengubah profile")
+      }
     })
   }
 
@@ -96,96 +82,47 @@ export function ProfileForm() {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="shadcn" {...field} disabled={isPending} />
               </FormControl>
               <FormDescription>
-                This is your public display name. It can be your real name or a
-                pseudonym. You can only change this once every 30 days.
+                Ini merupakan username yang akan digunakan untuk login anda.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+
         <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a verified email to display" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="m@example.com">m@example.com</SelectItem>
-                  <SelectItem value="m@google.com">m@google.com</SelectItem>
-                  <SelectItem value="m@support.com">m@support.com</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormDescription>
-                You can manage verified email addresses in your{" "}
-                <Link href="/examples/forms">email settings</Link>.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="bio"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Bio</FormLabel>
               <FormControl>
-                <Textarea
-                  placeholder="Tell us a little bit about yourself"
-                  className="resize-none"
+                <Input
                   {...field}
+                  placeholder="Masukkan email"
+                  disabled={isPending}
                 />
               </FormControl>
               <FormDescription>
-                You can <span>@mention</span> other users and organizations to
-                link to them.
+                Email ini akan digunakan untuk mengirimkan notifikasi, reset
+                password, dan lain-lain.
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <div>
-          {fields.map((field, index) => (
-            <FormField
-              control={form.control}
-              key={field.id}
-              name={`urls.${index}.value`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className={cn(index !== 0 && "sr-only")}>
-                    URLs
-                  </FormLabel>
-                  <FormDescription className={cn(index !== 0 && "sr-only")}>
-                    Add links to your website, blog, or social media profiles.
-                  </FormDescription>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+
+        <Button type="submit" disabled={isPending} className="w-fit">
+          {isPending && (
+            <Icons.spinner
+              className="mr-2 h-4 w-4 animate-spin"
+              aria-hidden="true"
             />
-          ))}
-          <Button
-            type="button"
-            variant="outline"
-            size="sm"
-            className="mt-2"
-            onClick={() => append({ value: "" })}
-          >
-            Add URL
-          </Button>
-        </div>
-        <Button type="submit">Update profile</Button>
+          )}
+          Update
+        </Button>
       </form>
     </Form>
   )
