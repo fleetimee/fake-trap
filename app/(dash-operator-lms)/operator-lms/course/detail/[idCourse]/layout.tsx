59ba0ee673d495c6fbf88e4f8f5c0f81e7fa7087
Novian Andika <novian.andika@students.amikom.ac.id>
2023-12-01 08:18:30 +0700

    Add accordion and scroll area components to course detail layout

--- app/(dash-operator-lms)/operator-lms/course/detail/[idCourse]/layout.tsx
+++ app/(dash-operator-lms)/operator-lms/course/detail/[idCourse]/layout.tsx
@@ -9,8 +9,15 @@ import { MotionDiv } from "@/components/framer-wrapper"
 import { Icons } from "@/components/icons"
 import { BreadCrumbs } from "@/components/pagers/breadcrumb"
 import { DashboardShell } from "@/components/shell"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Card } from "@/components/ui/card"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 interface CourseDetailLayoutProps {
   children: React.ReactNode
@@ -117,6 +124,7 @@ export default async function CourseDetailLayout({
         className="flex h-auto flex-col gap-4 px-2 lg:flex-row"
         id="scrollTarget"
       >
+        {/* Content Section */}
         <Card className="flex w-full basis-3/4 items-start justify-normal">
           <div className="flex w-full flex-col gap-6 p-4">
             <div className="flex flex-row items-center justify-between">
@@ -130,6 +138,47 @@ export default async function CourseDetailLayout({
               {children}
             </div>
           </div>
+        </Card>
+
+        {/* Sidebar Section */}
+        <Card className="flex h-[750px] basis-1/4 flex-col items-center justify-start">
+          <Tabs defaultValue="knowledge" className="w-full">
+            <TabsList className="w-full">
+              <TabsTrigger value="knowledge" className="w-full font-semibold">
+                Konten
+              </TabsTrigger>
+              <TabsTrigger value="course" className="w-full font-semibold">
+                Pelatihan
+              </TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="knowledge">
+              <ScrollArea className="h-700px w-full">
+                {course?.data?.section ? (
+                  <Accordion
+                    type="single"
+                    collapsible
+                    className="px-4"
+                    key={course.data.section[0].id_section.toString()}
+                    defaultValue={course.data.section[0].id_section.toString()}
+                  >
+                    {course.data.section.map((section) => (
+                      <AccordionItem
+                        key={section.id_section.toString()}
+                        value={section.id_section.toString()}
+                      >
+                        <AccordionTrigger className="font-heading text-base font-bold">
+                          {`${section.section_title}`}
+                        </AccordionTrigger>
+                      </AccordionItem>
+                    ))}
+                  </Accordion>
+                ) : null}
+              </ScrollArea>
+            </TabsContent>
+
+            <TabsContent value="course"></TabsContent>
+          </Tabs>
         </Card>
       </div>
     </DashboardShell>
