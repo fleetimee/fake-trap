504f3fc8468031110f511dfdf209b7b8c84744da
Novian Andika <novian.andika@students.amikom.ac.id>
2023-12-05 11:24:22 +0700

    Refactor OperatorLMSRolePage and add SidebarNavRole component

--- app/(dash-operator-lms)/operator-lms/roles/page.tsx
+++ app/(dash-operator-lms)/operator-lms/roles/page.tsx
@@ -1,3 +1,72 @@
-export default function OperatorLMSRolePage() {
-  return <div>Operator LMS Profile</div>
+import { JSX, SVGProps } from "react"
+import { Metadata } from "next"
+import { redirect } from "next/navigation"
+
+import { authOptions } from "@/lib/auth"
+import { getCurrentUser } from "@/lib/session"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Switch } from "@/components/ui/switch"
+
+export const metadata: Metadata = {
+  title: "Managemen Kewenangan",
+  description: "Managemen Kewenangan",
+}
+
+export default async function OperatorLMSRolePage() {
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/login")
+  }
+
+  return <Component />
+}
+
+export function Component() {
+  return (
+    <Card key="1" className="w-full max-w-lg">
+      <CardHeader className="border-dark-gray-300 border-b pb-4">
+        <div className="flex items-center">
+          <IconCookie className="mr-2" />
+          <CardTitle>Managemen Kewenangan</CardTitle>
+        </div>
+        <CardDescription>
+          Klik pada salah satu kewenangan untuk melihat detailnya.
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  )
+}
+
+function IconCookie(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5" />
+      <path d="M8.5 8.5v.01" />
+      <path d="M16 15.5v.01" />
+      <path d="M12 12v.01" />
+      <path d="M11 17v.01" />
+      <path d="M7 14v.01" />
+    </svg>
+  )
 }
--- app/(dash-operator-lms)/operator-lms/setting/_components/setting_sidebar.tsx
+++ app/(dash-operator-lms)/operator-lms/setting/_components/setting_sidebar.tsx
@@ -3,12 +3,9 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
+import { RoleListResData } from "@/types/role/res"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-
-
-
-
 
 interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
   items: {
@@ -41,6 +38,34 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
           )}
         >
           {item.title}
+        </Link>
+      ))}
+    </nav>
+  )
+}
+
+interface SidebarNavRoleProps {
+  items: RoleListResData[]
+}
+
+export function SidebarNavRole({ items }: SidebarNavRoleProps) {
+  const pathname = usePathname()
+
+  return (
+    <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
+      {items.map((item) => (
+        <Link
+          key={item.id_role}
+          href={`/operator-lms/roles/${item.role_name.toLowerCase()}`}
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            pathname === `/operator-lms/roles/${item.role_name.toLowerCase()}`
+              ? "bg-muted hover:bg-muted"
+              : "hover:bg-transparent hover:underline",
+            "justify-start"
+          )}
+        >
+          {item.role_name}
         </Link>
       ))}
     </nav>
