04c7f04fd6c1c55a06005d97f54cb1e77fe9cbba
Novian Andika <hello@fleetime.my.id>
2024-01-25 06:38:58 +0700

    Refactor UserAuthForm component

--- app/(dash-peserta)/peserta/page.tsx
+++ app/(dash-peserta)/peserta/page.tsx
@@ -1,23 +1,87 @@
 import { Metadata } from "next"
+import Link from "next/link"
 import { redirect } from "next/navigation"
-import { PartyPopper } from "lucide-react"
+import { ArrowRightIcon, PartyPopper } from "lucide-react"
 
 import { authOptions } from "@/lib/auth"
 import { getLoggedOnUser } from "@/lib/fetcher/auth-fetcher"
 import { getGlobalCount } from "@/lib/fetcher/menu-fetcher"
+import { getPesertaEnrolledCourses } from "@/lib/fetcher/users-fetcher"
 import { getCurrentUser } from "@/lib/session"
 import { dateNow, extractToken, getDayWithText } from "@/lib/utils"
-import { MotionDiv } from "@/components/framer-wrapper"
 import { DashboardHeader } from "@/components/header"
 import { Icons } from "@/components/icons"
 import { BreadCrumbs } from "@/components/pagers/breadcrumb"
 import { DashboardShell } from "@/components/shell"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { Widget } from "@/components/widget"
 
 export const metadata: Metadata = {
   title: "Dashboard",
 }
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
 
 export default async function PesertaPage() {
   const user = await getCurrentUser()
@@ -36,6 +100,17 @@ export default async function PesertaPage() {
   const globalCount = await getGlobalCount({
     token: user?.token,
   })
+
+  const course = await getPesertaEnrolledCourses({
+    token: user?.token,
+    uuid: tokenExtracted?.id,
+    limit: 5,
+    page: 1,
+    orderBy: "created_at",
+    sortBy: "desc",
+  })
+
+  console.log(course)
 
   return (
     <DashboardShell>
@@ -91,6 +166,100 @@ export default async function PesertaPage() {
           title="Pelatihan"
           subtitle={globalCount.data?.course_count.toString()}
         />
+      </div>
+
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="h-fit lg:col-span-3">
+          <CardHeader>
+            <CardTitle>Pelatihan</CardTitle>
+            <CardDescription>
+              Berikut adalah pelatihan yang kamu ikuti
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="flex  flex-col gap-4">
+            {course.data.length > 0 ? (
+              <div>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Nama Pelatihan</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {course.data.map((invoice) => (
+                      <TableRow key={invoice.id_course}>
+                        <TableCell className="font-medium">
+                          {invoice.course_name}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+
+                <Link href="/peserta/course">
+                  <Button variant="outline" className="w-full">
+                    Lihat Semua
+                    <ArrowRightIcon
+                      className="size-4 ml-2"
+                      aria-hidden="true"
+                    />
+                  </Button>
+                </Link>
+              </div>
+            ) : (
+              <div className="mx-auto flex flex-col items-center justify-center py-6">
+                <Icons.course className="h-20 w-20 text-gray-400" />
+                <p className="text-gray-400">
+                  Belum ada pelatihan yang diikuti
+                </p>
+              </div>
+            )}
+          </CardContent>
+        </Card>
+
+        <Card className="h-fit lg:col-span-4">
+          <CardHeader>
+            <CardTitle>Threads Activity</CardTitle>
+            <CardDescription>
+              Here is a list of recent threads activity
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <Table>
+              <TableCaption>A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Invoice</TableHead>
+                  <TableHead>Status</TableHead>
+                  <TableHead>Method</TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {invoices.map((invoice) => (
+                  <TableRow key={invoice.invoice}>
+                    <TableCell className="font-medium">
+                      {invoice.invoice}
+                    </TableCell>
+                    <TableCell>{invoice.paymentStatus}</TableCell>
+                    <TableCell>{invoice.paymentMethod}</TableCell>
+                    <TableCell className="text-right">
+                      {invoice.totalAmount}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={3}>Total</TableCell>
+                  <TableCell className="text-right">$2,500.00</TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </CardContent>
+        </Card>
       </div>
     </DashboardShell>
   )
--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -27,7 +27,7 @@ type Inputs = z.infer<typeof authSchema>
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+export function UserAuthForm({ className }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = useState(false)
 
   const [isPending, startTransition] = useTransition()
@@ -45,14 +45,6 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const searchParams = useSearchParams()
 
   const router = useRouter()
-
-  const email = useRef("")
-  const password = useRef("")
-
-  // async function onSubmit(event: SyntheticEvent) {
-  //   event.preventDefault()
-  //   setIsLoading(true)
-  // }
 
   async function onSubmit(data: Inputs) {
     startTransition(async () => {
@@ -88,102 +80,6 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   }
 
   return (
-    // <div className={cn("grid gap-6", className)} {...props}>
-
-    //   <form onSubmit={onSubmit}>
-    //     <div className="grid gap-2 space-y-3">
-    //       <div className="grid gap-4 pb-2">
-    //         <Label htmlFor="email">Email</Label>
-    //         <Input
-    //           id="email"
-    //           type="text"
-    //           autoCapitalize="none"
-    //           disabled={isLoading}
-    //           onChange={(e) => {
-    //             email.current = e.target.value
-    //           }}
-    //         />
-    //       </div>
-    //       <div className="grid gap-4 pb-2">
-    //         <Label htmlFor="password">Password</Label>
-    //         <Input
-    //           id="password"
-    //           placeholder=""
-    //           type="password"
-    //           autoCapitalize="none"
-    //           disabled={isLoading}
-    //           onChange={(e) => {
-    //             password.current = e.target.value
-    //           }}
-    //         />
-    //       </div>
-    //       <Turnstile
-    //         siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY || ""}
-    //         className="h-12 w-full"
-    //         onSuccess={async (token) => {
-    //           const response = await fetch(
-    //             `${process.env.NEXT_PUBLIC_BASE_URL}/verifyTurnstile`,
-    //             {
-    //               method: "POST",
-    //               headers: {
-    //                 "Content-Type": "application/json",
-    //               },
-    //               body: JSON.stringify({
-    //                 token: token,
-    //               }),
-    //             }
-    //           )
-
-    //           if (response.ok) {
-    //             setCaptchaVerified(true)
-    //           } else {
-    //             sonnerToast.error("Perhatian", {
-    //               description: "Captcha tidak valid",
-    //             })
-    //           }
-    //         }}
-    //       />
-
-    //       <Button
-    //         disabled={isLoading || !isCaptchaVerified} // Disable button if loading or captcha not verified
-    //         onClick={() =>
-    //           signIn("credentials", {
-    //             email: email.current,
-    //             password: password.current,
-    //             callbackUrl: searchParams.get("from") || "/",
-    //             redirect: false,
-    //           }).then((res) => {
-    //             console.log(res)
-
-    //             if (!res?.ok) {
-    //               setIsLoading(false)
-
-    //               sonnerToast.error("Perhatian", {
-    //                 description: `${res?.error}`,
-    //               })
-    //             } else {
-    //               setIsLoading(false)
-
-    //               router.push("/login")
-
-    //               router.refresh()
-
-    //               sonnerToast.success("Berhasil", {
-    //                 description: "Anda berhasil masuk",
-    //               })
-    //             }
-    //           })
-    //         }
-    //       >
-    //         {isLoading && (
-    //           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-    //         )}
-    //         Masuk
-    //       </Button>
-    //     </div>
-    //   </form>
-    // </div>
-
     <Form {...form}>
       <form
         className="grid gap-4"
