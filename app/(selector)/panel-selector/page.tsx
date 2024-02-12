d8259e5aec3afafc42dcec37310656bbdb6f02c0
Novian Andika <novian.andika@students.amikom.ac.id>
2024-02-12 08:03:19 +0700

    Fix formatting issue in PanelSelector component

--- app/(selector)/panel-selector/page.tsx
+++ app/(selector)/panel-selector/page.tsx
@@ -41,8 +41,8 @@ export default async function PanelSelector() {
       className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
     >
       <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-        <Balancer>
-          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+        <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+          <Balancer>
             Halo, {tokenExtracted.name}
             <span
               className="ml-4 inline-block"
@@ -53,8 +53,8 @@ export default async function PanelSelector() {
                 className="inline-block size-14"
               />
             </span>
-          </h2>
-        </Balancer>
+          </Balancer>
+        </h2>
 
         <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
           Silahkan masuk ke panel yang anda inginkan sesuai dengan hak akses
