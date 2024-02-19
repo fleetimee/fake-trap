86ddcd918b5d98fcd0b11a342ceaf2259e813687
Novian Andika <novian.andika@students.amikom.ac.id>
2024-02-19 11:35:56 +0700

    Add leaderboard page for exercise detail

---
+++ app/(dash-pemateri-divisi)/pemateri-divisi/exercise/detail/[idExercise]/leaderboard/page.tsx
@@ -0,0 +1,68 @@
+import { Suspense } from "react"
+import { Metadata } from "next"
+import { redirect } from "next/navigation"
+
+import { authOptions } from "@/lib/auth"
+import { getQuizLeaderboard } from "@/lib/fetcher/exercise-fetcher"
+import { getCurrentUser } from "@/lib/session"
+import { DataTableSkeleton } from "@/components/data-table/data-table-skeleton"
+import { DashboardShell } from "@/components/shell"
+import { QuizLeaderboardTableShell } from "@/components/shell/quiz-leaderboard-table-shell"
+
+export const metadata: Metadata = {
+  title: "Leaderboard",
+  description: "Leaderboard",
+}
+
+interface ExerciseDetailLeaderboardPageProps {
+  params: {
+    idExercise: string
+  }
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+export default async function ExerciseDetailLeaderboardPage({
+  params,
+  searchParams,
+}: ExerciseDetailLeaderboardPageProps) {
+  const user = await getCurrentUser()
+
+  const { page, per_page, sort } = searchParams ?? {}
+
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/login")
+  }
+
+  // Initial value
+  const pageInitial = typeof page === "string" ? parseInt(page) : 1
+  const limitInitial = typeof per_page === "string" ? parseInt(per_page) : 10
+  const orderByInitial = typeof sort === "string" ? sort : "desc"
+  const sortByInitial = typeof sort === "string" ? sort : "position"
+
+  // split sort
+  const sortBy = sortByInitial.split(".")[0]
+  const orderBy = orderByInitial.split(".")[1]
+
+  const leaderboard = await getQuizLeaderboard({
+    idExercise: params.idExercise,
+    limit: limitInitial,
+    page: pageInitial,
+    sortBy: sortBy,
+    orderBy: orderBy,
+    token: user?.token,
+  })
+
+  return (
+    <DashboardShell>
+      <Suspense fallback={<DataTableSkeleton columnCount={10} />}>
+        <QuizLeaderboardTableShell
+          data={leaderboard?.data}
+          pageCount={leaderboard.totalPage}
+          idExercise={params.idExercise}
+        />
+      </Suspense>
+    </DashboardShell>
+  )
+}
