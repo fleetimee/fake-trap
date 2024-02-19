29f1cea8cfd64aabcaa418e23d3b8e13347bd9f7
Novian Andika <novian.andika@students.amikom.ac.id>
2024-02-19 08:54:26 +0700

    Add getUserPretestCheck function and update CourseDetailPage component

--- app/(dash-peserta)/peserta/course/detail/[idCourse]/page.tsx
+++ app/(dash-peserta)/peserta/course/detail/[idCourse]/page.tsx
@@ -4,8 +4,10 @@ import { notFound, redirect } from "next/navigation"
 import { authOptions } from "@/lib/auth"
 import { CourseAvailability } from "@/lib/enums/status"
 import { getOneCourse } from "@/lib/fetcher/course-fetcher"
+import { getUserPretestCheck } from "@/lib/fetcher/users-fetcher"
 import { getCurrentUser } from "@/lib/session"
-import { getCourseStatus } from "@/lib/utils"
+import { extractToken, getCourseStatus } from "@/lib/utils"
+import { Icons } from "@/components/icons"
 
 interface CourseDetailPageProps {
   params: {
@@ -22,6 +24,8 @@ export default async function CourseDetailPage({
     redirect(authOptions?.pages?.signIn || "/login")
   }
 
+  const tokenExtracted = extractToken(user?.token)
+
   const course = await getOneCourse({
     token: user?.token,
     idCourse: params.idCourse,
@@ -36,7 +40,13 @@ export default async function CourseDetailPage({
     return notFound()
   }
 
-  return (
+  const checkUserPretest = await getUserPretestCheck({
+    idCourse: params.idCourse,
+    token: user?.token,
+    userUuid: tokenExtracted?.id,
+  })
+
+  return checkUserPretest.data ? (
     <Image
       src={`${process.env.NEXT_PUBLIC_BASE_URL}${course?.data?.image}`}
       alt={course?.data?.course_name}
@@ -44,5 +54,23 @@ export default async function CourseDetailPage({
       width={1280}
       height={720}
     />
+  ) : (
+    <div className="relative">
+      <Image
+        src={`${process.env.NEXT_PUBLIC_BASE_URL}${course?.data?.image}`}
+        alt={course?.data?.course_name}
+        className="aspect-video rounded-none object-cover shadow-md md:rounded-lg "
+        width={1280}
+        height={720}
+      />
+
+      <div className="absolute inset-0 flex  flex-col items-center justify-center bg-black/50 grayscale">
+        <Icons.lock className="size-16 text-white" />
+        <p className="mt-4 max-w-md text-center text-white">
+          Silahkan kerjakan Pretest terlebih dahulu, untuk membuka materi
+          pelatihan.
+        </p>
+      </div>
+    </div>
   )
 }
