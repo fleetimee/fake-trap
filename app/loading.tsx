8555884c096db1a19329fc4f2e3bfab618da424d
Novian Andika <novian.andika@students.amikom.ac.id>
2024-02-20 13:30:04 +0700

    Refactor loading component and update next.config

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import Loader from "@/public/lottie/loading1.json"
+import Lottie from "lottie-react"
 
 import { LottieClient } from "@/components/lottie-anim"
 
 export default function GlobalLoadingPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <LottieClient animationData={Loader} className="size-1/6" />
+      {/* <LottieClient animationData={Loader} className="size-1/6" /> */}
+
+      <Lottie animationData={Loader} className="size-1/6" />
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  swcMinify: false,
   reactStrictMode: true,
   publicRuntimeConfig: {
     basePath: "",
