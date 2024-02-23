638cde8d09404cff7e501e2d69249ae1799b8e07
Novian Andika <novian.andika@students.amikom.ac.id>
2024-02-23 13:55:49 +0700

    Refactor loading page layout and add BPD logo and spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,16 +1,24 @@
 "use client"
 
+import Image from "next/image"
+import BpdLogo from "@/public/images/logo.png"
 import Loader from "@/public/lottie/loading1.json"
 import Lottie from "lottie-react"
 
+import { Icons } from "@/components/icons"
 import { LottieClient } from "@/components/lottie-anim"
 
 export default function GlobalLoadingPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <LottieClient animationData={Loader} className="size-1/6" />
+    // <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-2 space-y-4">
+      {/* <LottieClient animationData={Loader} className="size-1/6" /> */}
 
       {/* <Lottie animationData={Loader} className="size-1/6" /> */}
+
+      <Image src={BpdLogo} alt="BPD Logo" className="ml-12 w-1/6" />
+
+      <Icons.spinner className="w-1/6 animate-spin" />
 
       {/* <p className="text-2xl font-bold text-gray-500">...</p> */}
     </div>
