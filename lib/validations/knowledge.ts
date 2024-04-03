dfeb5a4d32ac1b140364c5a2fa5068f823b93915
Novian Andika <novian.andika@students.amikom.ac.id>
2024-04-03 13:42:51 +0700

    Refactor image validation in category, course, and knowledge schemas

--- lib/validations/knowledge.ts
+++ lib/validations/knowledge.ts
@@ -15,28 +15,7 @@ export const knowledgeSchema = z.object({
   Status: z.string().min(1, {
     message: "Status tidak boleh kosong",
   }),
-  image: z
-    .instanceof(File, {
-      message: "Gambar tidak boleh kosong",
-    })
-    .refine(
-      (file) => {
-        if (file) {
-          const validFileTypes = [
-            "image/jpeg",
-            "image/png",
-            "image/bmp",
-            "image/jpg",
-          ]
-          return file.size < 1000000 && validFileTypes.includes(file.type)
-        }
-        return true
-      },
-      {
-        message:
-          "Ukuran file tidak boleh lebih dari 1MB dan harus berformat jpg, png, bmp, atau jpeg",
-      }
-    ),
+  image: z.union([z.instanceof(File), z.null()]).optional(),
   IdCategory: z.number().min(1, {
     message: "Modul tidak boleh kosong",
   }),
