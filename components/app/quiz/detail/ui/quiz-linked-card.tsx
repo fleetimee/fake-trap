82988124f3f5f8b487f7c872e37528a714953ac3
Novian Andika <novian.andika@students.amikom.ac.id>
2024-04-19 09:02:36 +0700

    Update UI components and fix layout in quiz-linked-card.tsx

--- components/app/quiz/detail/ui/quiz-linked-card.tsx
+++ components/app/quiz/detail/ui/quiz-linked-card.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import Link from "next/link"
-
 import { QuizLinkedListData } from "@/types/quiz/res"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
 import {
   Card,
   CardContent,
@@ -17,24 +17,84 @@ interface QuizLinkedCardProps {
 }
 
 export function QuizLinkedCard({ code, data }: QuizLinkedCardProps) {
+  const linkedCheck = code
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle className="text-lg">Link Kuis Ke Pembelajaran</CardTitle>
+        <CardTitle className="text-lg">Hubungkan ke pembelajaran</CardTitle>
         <CardDescription>
-          Apakah kuis ini sudah di hubungkan ke pembelajaran ?
+          Apakah ujian ini sudah di hubungkan ke pembelajaran ?
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <div className="flex items-start space-x-4">
-          <div>
-            {code !== 404 ? (
-              <p className="font-heading text-lg underline hover:text-blue-600">
-                {data.course_name}
-              </p>
-            ) : (
-              <p className="font-heading text-lg">Belum ada</p>
+        <div className="flex  items-start  space-x-4">
+          <div
+            className={cn(
+              "-mx-2 flex items-start space-x-4 rounded-md  p-2 transition-all",
+              linkedCheck
+                ? "bg-accent"
+                : "hover:bg-accent hover:text-accent-foreground"
             )}
+          >
+            <Icons.check
+              className={cn(
+                "mt-px h-5 w-5",
+                !linkedCheck ? "text-foreground" : "text-green-600"
+              )}
+            />
+            <div className="space-y-1">
+              <p
+                className={cn(
+                  "text-sm font-medium leading-none",
+                  !linkedCheck ? "line-through" : ""
+                )}
+              >
+                Sudah
+              </p>
+              <p
+                className={cn(
+                  "text-sm text-muted-foreground",
+                  !linkedCheck ? "line-through" : ""
+                )}
+              >
+                Ujian ini sudah dihubungkan ke pembelajaran.
+              </p>
+            </div>
+          </div>
+          <div
+            className={cn(
+              "-mx-2 flex items-start space-x-4 rounded-md  p-2 transition-all",
+              !linkedCheck
+                ? "bg-accent"
+                : "hover:bg-accent hover:text-accent-foreground"
+            )}
+          >
+            <Icons.close
+              className={cn(
+                "mt-px h-5 w-5",
+                linkedCheck ? "text-foreground" : "text-red-600"
+              )}
+            />
+            <div className="space-y-1">
+              <p
+                className={cn(
+                  "text-sm font-medium leading-none",
+                  linkedCheck ? "line-through" : ""
+                )}
+              >
+                Belum
+              </p>
+
+              <p
+                className={cn(
+                  "text-sm text-muted-foreground",
+                  linkedCheck ? "line-through" : ""
+                )}
+              >
+                Ujian ini belum dihubungkan ke pembelajaran.
+              </p>
+            </div>
           </div>
         </div>
       </CardContent>
