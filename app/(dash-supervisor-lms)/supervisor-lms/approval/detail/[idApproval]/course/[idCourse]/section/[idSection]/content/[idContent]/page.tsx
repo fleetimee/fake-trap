e921a9253258f77d3089909ee04a99ee9a5ee634
Novian Andika <novian.andika@students.amikom.ac.id>
2024-06-03 09:01:18 +0700

    feat: fix broken page

--- app/(dash-supervisor-lms)/supervisor-lms/approval/detail/[idApproval]/course/[idCourse]/section/[idSection]/content/[idContent]/page.tsx
+++ app/(dash-supervisor-lms)/supervisor-lms/approval/detail/[idApproval]/course/[idCourse]/section/[idSection]/content/[idContent]/page.tsx
@@ -12,6 +12,7 @@ import { YoutubeFrame } from "@/components/frame/youtube-frame"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
+import {FileFrame} from "@/components/frame/file-frame";
 
 interface CourseContentPageProps {
   params: {
@@ -53,87 +54,14 @@ export default async function CourseContentPage({
 
   if (file) {
     return (
-      <div className="flex flex-col items-start justify-center gap-6">
-        <h1 className="text-4xl font-bold  ">{content.data.content_title}</h1>
-        <Separator />
-
-        <p className="text-xl italic ">
-          Terdapat {content.data.files?.length} file yang dapat diunduh untuk di
-          baca
-        </p>
-
-        <div className="grid grid-cols-1 items-center justify-center gap-8  xl:grid-cols-2">
-          {content.data.files &&
-            content.data.files.map((file) => (
-              <Card
-                key={file.id_content_file}
-                className="mx-auto w-80 overflow-hidden rounded-lg shadow-lg transition-all duration-200 hover:shadow-xl"
-              >
-                <Image
-                  alt="Profile picture"
-                  className="w-full object-cover"
-                  height="320"
-                  src="/images/placeholder.svg"
-                  style={{
-                    aspectRatio: "320/320",
-                    objectFit: "cover",
-                  }}
-                  width="320"
-                />
-                <CardContent className="p-4">
-                  <h2 className="text-2xl font-bold transition-all duration-200 hover:text-gray-700">
-                    {file.file_type}
-                  </h2>
-                  <h3 className="text-gray-500 transition-all duration-200 hover:text-gray-600">
-                    {Number(file.file_size)
-                      ? (Number(file.file_size) / 1024 / 1024).toFixed(2)
-                      : "N/A"}{" "}
-                    MB
-                  </h3>
-                  <p className="mt-2 text-gray-600 transition-all duration-200 hover:text-gray-700">
-                    {file.file_path.split("/").pop()}
-                  </p>
-                  <div className="mt-4 flex space-x-2">
-                    <Link
-                      href={`${process.env.NEXT_PUBLIC_BASE_URL}/${file.file_path}`}
-                      target="_blank"
-                      className={buttonVariants({
-                        size: "sm",
-                        className:
-                          "w-full transition-all duration-200 hover:bg-gray-700 hover:text-white",
-                      })}
-                    >
-                      Download
-                    </Link>
-                    <Button
-                      className="w-full transition-all duration-200 hover:border-gray-700 hover:text-gray-700"
-                      size="sm"
-                      variant="outline"
-                    >
-                      <Link
-                        // href={`/pemateri-divisi/course/detail/${
-                        //   params.idCourse
-                        // }/section/${params.idSection}/content/${
-                        //   params.idContent
-                        // }/render/${file.file_path.split("/").pop()}`}
-
-                        href={`/supervisor-lms/approval/detail/${
-                          params.idApproval
-                        }/course/${params.idCourse}/section/${
-                          params.idSection
-                        }/content/${params.idContent}/render/${file.file_path
-                          .split("/")
-                          .pop()}`}
-                      >
-                        Preview
-                      </Link>
-                    </Button>
-                  </div>
-                </CardContent>
-              </Card>
-            ))}
-        </div>
-      </div>
+      <FileFrame
+        content={content}
+        params={{
+          detail: params.idCourse,
+          idSection: params.idSection,
+          idContent: params.idContent,
+        }}
+      />
     )
   }
 
