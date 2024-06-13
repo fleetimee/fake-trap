4b6f7781c5bbc51a28959b4a14d52a69c56d69bb
Novian Andika <novian.andika@students.amikom.ac.id>
2024-06-13 12:56:17 +0700

    refactor: Update getPesertaCount function to include userId parameter

--- app/(dash-peserta)/peserta/page.tsx
+++ app/(dash-peserta)/peserta/page.tsx
@@ -64,6 +64,7 @@ export default async function PesertaPage() {
       }),
       getPesertaCount({
         token: user?.token,
+        userId: tokenExtracted?.id,
       }),
       getPesertaEnrolledCourses({
         token: user?.token,
