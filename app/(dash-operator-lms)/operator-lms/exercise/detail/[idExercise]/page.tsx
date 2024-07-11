5ebbe73a7f10a8a95d4969ecce70c376b3f60acf
Novian Andika <novian.andika@students.amikom.ac.id>
2024-07-12 00:05:17 +0700

    refactor: Add QuizOpenClosedCard component to ExerciseDetailPage

--- app/(dash-operator-lms)/operator-lms/exercise/detail/[idExercise]/page.tsx
+++ app/(dash-operator-lms)/operator-lms/exercise/detail/[idExercise]/page.tsx
@@ -18,6 +18,7 @@ import {
   QuizTypeCard,
   QuizUserCountCard,
 } from "@/components/app/quiz/detail/ui"
+import { QuizOpenClosedCard } from "@/components/app/quiz/detail/ui/quiz-open-closed-card"
 import { MotionDiv } from "@/components/framer-wrapper"
 
 interface ExerciseDetailPageProps {
@@ -122,12 +123,15 @@ export default async function ExerciseDetailPage({
       <MotionDiv className="child grid grid-cols-1" variants={childrenVariant}>
         <QuizLinkedCard code={exerciseLinked.code} data={exerciseLinked.data} />
       </MotionDiv>
-      <div className="grid grid-flow-row gap-4 md:grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-flow-row gap-4 md:grid-cols-1 lg:grid-cols-3">
         <MotionDiv className="child" variants={childrenVariant}>
           <QuizDescriptionCard data={exercise.data} />
         </MotionDiv>
         <MotionDiv className="child" variants={childrenVariant}>
           <QuizCalendarCard data={exercise.data} />
+        </MotionDiv>
+        <MotionDiv className="child" variants={childrenVariant}>
+          <QuizOpenClosedCard data={exercise.data} />
         </MotionDiv>
       </div>
       <MotionDiv />
