43b04cfd65581c476ec9787e4464c77581c2bdcb
Novian Andika <novian.andika@students.amikom.ac.id>
2024-07-23 11:57:22 +0700

    feat: Fetch and display audit trail data on AdminAuditTrailPage

--- app/(dash-administrator)/administrator/audit-trail/page.tsx
+++ app/(dash-administrator)/administrator/audit-trail/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 import { redirect } from "next/navigation"
 
 import { authOptions } from "@/lib/auth"
+import { getAuditTrail } from "@/lib/fetcher/audit-trail-fetcher"
 import { getCurrentUser } from "@/lib/session"
 import { DashboardHeader } from "@/components/header"
 import { BreadCrumbs } from "@/components/pagers/breadcrumb"
@@ -26,6 +27,34 @@ export default async function AdminAuditTrailPage({
   if (!user) {
     redirect(authOptions?.pages?.signIn || "/login")
   }
+
+  const { page, per_page, sort, method, from, to } = searchParams ?? {}
+
+  const pageInitial = typeof page === "string" ? parseInt(page) : 1
+  const limitInitial = typeof per_page === "string" ? parseInt(per_page) : 10
+
+  const sortByInitial = typeof sort === "string" ? sort : "id"
+  const orderByInitial = typeof sort === "string" ? sort : "desc"
+
+  const methodInitial = typeof method === "string" ? method : ""
+  const fromInitial = typeof from === "string" ? from : ""
+  const toInitial = typeof to === "string" ? to : ""
+
+  const sortField = sortByInitial.split(".")[0]
+  const sortOrder = orderByInitial.split(".")[1]
+
+  const auditTrail = await getAuditTrail({
+    token: user?.token,
+    page: pageInitial,
+    limit: limitInitial,
+    method: methodInitial,
+    sortField: sortField,
+    orderBy: sortOrder,
+    from: fromInitial,
+    to: toInitial,
+  })
+
+  console.log(auditTrail)
 
   return (
     <DashboardShell>
