e542aa6cf2c4a28673bf36fb5742acb103bdfb02
Novian Andika <novian.andika@students.amikom.ac.id>
2024-07-23 07:59:12 +0700

    refactor: Add StrukturOrganisasiSyncButton component for manual synchronization of organizational structure data

--- app/(dash-administrator)/administrator/struktur-organisasi/_components/struktur-organisasi-sync-button.tsx
+++ app/(dash-administrator)/administrator/struktur-organisasi/_components/struktur-organisasi-sync-button.tsx
@@ -1,17 +1,32 @@
 "use client"
 
-import { useTransition } from "react"
+import { useState, useTransition } from "react"
 import { useRouter } from "next/navigation"
+import { FolderSyncIcon } from "lucide-react"
 import { useSession } from "next-auth/react"
 import { toast as sonnerToast } from "sonner"
 
 import { syncStrukturOrganisasi } from "@/lib/fetcher/struktur-organisasi-fetcher"
-import { AlertDialogAction } from "@/components/ui/alert-dialog"
+import { Icons } from "@/components/icons"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
 
 export function StrukturOrganisasiSyncButton() {
   const { data: session } = useSession()
 
   const [isPending, startTransition] = useTransition()
+
+  const [open, setOpen] = useState(false)
 
   const router = useRouter()
 
@@ -20,26 +35,60 @@ export function StrukturOrganisasiSyncButton() {
   }
 
   return (
-    <AlertDialogAction
-      disabled={isPending}
-      onClick={async (event) => {
-        event.preventDefault()
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          className="ml-auto hidden h-8 lg:flex"
+        >
+          <FolderSyncIcon className="mr-2 h-4 w-4" />
+          Sync
+        </Button>
+      </AlertDialogTrigger>
 
-        startTransition(async () => {
-          const res = await syncStrukturOrganisasi({
-            token: session.user.token,
-          })
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Data Struktur Organisasi akan di Sync secara manual
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Data Struktur Organisasi akan di Sync secara manual, pastikan
+            koneksi internet stabil
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={isPending}
+            onClick={async (event) => {
+              event.preventDefault()
 
-          if (res) {
-            sonnerToast.success("Proses sinkronisasi berhasil")
-            router.refresh()
-          } else {
-            sonnerToast.error("Gagal sinkronisasi")
-          }
-        })
-      }}
-    >
-      {isPending ? "Sedang sinkronisasi..." : "Sinkronisasi"}
-    </AlertDialogAction>
+              startTransition(async () => {
+                const res = await syncStrukturOrganisasi({
+                  token: session.user.token,
+                })
+
+                if (res.code === 200) {
+                  sonnerToast.success("Proses sinkronisasi berhasil")
+                  setOpen(false)
+                  router.push(
+                    "/administrator/struktur-organisasi?isSuccessSync=true"
+                  )
+                  router.refresh()
+                } else {
+                  sonnerToast.error("Gagal sinkronisasi")
+                }
+              })
+            }}
+          >
+            {isPending && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            <span>Sync</span>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
