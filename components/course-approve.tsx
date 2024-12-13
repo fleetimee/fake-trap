5c810d4b217e77650ef515fbe71932ffcd533816
Novian Andika <novian.andika@students.amikom.ac.id>
2024-12-13 11:39:43 +0700

    feat: Replace grid layout with flex column for improved responsiveness in course approval component

--- components/course-approve.tsx
+++ components/course-approve.tsx
@@ -186,7 +186,8 @@ export function CourseApproves({ approvals, pageCount }: ApprovesProps) {
         <DateRangePicker />
       </div>
 
-      <div className="mx-auto grid grid-cols-1 items-center justify-between gap-8 xl:grid-cols-3">
+      {/* Replace the grid with flex column */}
+      <div className="flex flex-col space-y-4">
         {approvals?.map((request) => (
           <PengajuanCard
             key={request.id_approval}
